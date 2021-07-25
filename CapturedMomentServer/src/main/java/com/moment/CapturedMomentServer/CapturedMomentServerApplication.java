1f8c847f842fb1a16e143114f24e4f36fe79ea14
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-25 21:37:36 +0900

    [#4] 프로젝트 생성

---
+++ CapturedMomentServer/src/main/java/com/moment/CapturedMomentServer/CapturedMomentServerApplication.java
@@ -0,0 +1,13 @@
+package com.moment.CapturedMomentServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapturedMomentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapturedMomentServerApplication.class, args);
+	}
+
+}
