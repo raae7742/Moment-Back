2ccc9de4862b4addb5cc1ace19b40d383025b003
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-08-02 16:47:29 +0900

    [#6] 전체 중 5000 (5km) 이내 마커 위치만 반환 구현

--- CapturedMomentServer/src/main/java/com/moment/CapturedMomentServer/controller/SpotController.java
+++ CapturedMomentServer/src/main/java/com/moment/CapturedMomentServer/controller/SpotController.java
@@ -2,11 +2,13 @@ package com.moment.CapturedMomentServer.controller;
 
 /* /spots 김혜수 2021-08-02*/
 
+import antlr.PreservingFileWriter;
 import com.moment.CapturedMomentServer.domain.Spot;
 import com.moment.CapturedMomentServer.repository.SpotRepository;
 import com.moment.CapturedMomentServer.response.JSONResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,17 +19,12 @@ public class SpotController {
 
     private final SpotRepository spotRepository;
 
-//    @GetMapping("/spots")
-//    public List<Spot> readSpot(@RequestParam double latitude, @RequestParam double longitude) {
-//        return spotRepository.findAll();
-//    }
-
     @GetMapping("/spots")
-    public JSONResponse<List<Spot>> getSpots(){
+    public JSONResponse<List<Spot>> getSpots(@RequestParam double latitude, @RequestParam double longitude){
         JSONResponse<List<Spot>> response = new JSONResponse<>();
         response.setStatusCode(200);
         response.setMessage("spot 조회 성공");
-        response.setData(spotRepository.findAll());
+        response.setData(spotRepository.datas(latitude, longitude));
 
         return response;
     }
