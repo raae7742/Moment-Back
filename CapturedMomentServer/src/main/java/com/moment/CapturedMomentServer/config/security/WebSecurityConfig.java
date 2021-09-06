bcf8beb3cb822111bfa58a82c4f8e962a8eb452b
Hyeonae Jang <aeae_1@naver.com>
2021-09-06 23:32:24 +0900

    [#24] Swagger
    Swagger로 API 문서 자동화

--- CapturedMomentServer/src/main/java/com/moment/CapturedMomentServer/config/security/WebSecurityConfig.java
+++ CapturedMomentServer/src/main/java/com/moment/CapturedMomentServer/config/security/WebSecurityConfig.java
@@ -44,6 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(
                         "/h2-console/**"
                         ,"/favicon.ico"
+                        ,"/swagger-ui.html/"
                 );
     }
 
