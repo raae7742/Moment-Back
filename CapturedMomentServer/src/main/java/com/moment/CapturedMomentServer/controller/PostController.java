0df5636d4cc6f76481c56713853fb0887eef26af
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-09-13 17:36:33 +0900

    [#17] 유저 게시물 조회 구현

--- CapturedMomentServer/src/main/java/com/moment/CapturedMomentServer/controller/PostController.java
+++ CapturedMomentServer/src/main/java/com/moment/CapturedMomentServer/controller/PostController.java
@@ -7,11 +7,9 @@ import com.moment.CapturedMomentServer.repository.PostTagRepository;
 import com.moment.CapturedMomentServer.repository.SpotRepository;
 import com.moment.CapturedMomentServer.response.JSONResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.parameters.P;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,10 +24,30 @@ public class PostController {
     public JSONResponse<Post> createPosts(@RequestHeader("X-AUTH-TOKEN") String token,
                                           @RequestBody PostRequestDto requestDto) {
 
-        Spot s = spotRepository.findByLatitudeAndLongitude(requestDto.getLatitude(), requestDto.getLongitude());
-        if (s != null){
-            Post post = new Post(requestDto, s.getId(), jwtTokenProvider.getUserEmail(token));
-            postRepository.save(post);
+//        Spot s = spotRepository.findByLatitudeAndLongitude(requestDto.getLatitude(), requestDto.getLongitude());
+//        if (s != null){
+//            // 이미 테이블에 위도, 경도 있는 경우
+//            Post post = new Post(requestDto, s.getId(), jwtTokenProvider.getUserEmail(token));
+//            postRepository.save(post);
+//            for(int i=0; i<requestDto.getTag().size(); i++){
+//                PostTag tag = new PostTag(post.getId(), requestDto.getTag().get(i));
+//                postTagRepository.save(tag);
+//            }
+//        }
+//        else {
+//            // 테이블에 위도, 경도 없는 경우
+//            Spot spot = new Spot(requestDto.getLatitude(), requestDto.getLongitude());
+//            Post post = new Post(requestDto, spotRepository.save(spot).getId(), jwtTokenProvider.getUserEmail(token));
+//            for(int i=0; i<requestDto.getTag().size(); i++){
+//                PostTag tag = new PostTag(post.getId(), requestDto.getTag().get(i));
+//                postTagRepository.save(tag);
+//            }
+//        }
+
+        if (spotRepository.existsByLatitudeAndLongitude(requestDto.getLatitude(), requestDto.getLongitude())){
+            // 이미 테이블에 그 위도, 경도가 있으면 -> 아이디 찾아오기..
+            Post post = new Post(requestDto, spotRepository.spotIdGet(requestDto.getLatitude(), requestDto.getLongitude()), jwtTokenProvider.getUserEmail(token));
+            Post responsePost = postRepository.save(post);
             for(int i=0; i<requestDto.getTag().size(); i++){
                 PostTag tag = new PostTag(post.getId(), requestDto.getTag().get(i));
                 postTagRepository.save(tag);
@@ -37,42 +55,35 @@ public class PostController {
 
         }
         else {
+            // 없으면
             Spot spot = new Spot(requestDto.getLatitude(), requestDto.getLongitude());
             Post post = new Post(requestDto, spotRepository.save(spot).getId(), jwtTokenProvider.getUserEmail(token));
+            Post responsePost = postRepository.save(post);
             for(int i=0; i<requestDto.getTag().size(); i++){
-                PostTag tag = new PostTag(post.getId(), requestDto.getTag().get(i));
+                PostTag tag = new PostTag(post.getId(), requestDto.getTag().indexOf(i) + 2);
                 postTagRepository.save(tag);
             }
-        }
 
-//        if (spotRepository.existsByLatitudeAndLongitude(requestDto.getLatitude(), requestDto.getLongitude())){
-//            // 이미 테이블에 그 위도, 경도가 있으면 -> 아이디 찾아오기..
-//            Post post = new Post(requestDto, spotRepository.spotIdGet(requestDto.getLatitude(), requestDto.getLongitude()), jwtTokenProvider.getUserEmail(token));
-//            Post responsePost = postRepository.save(post);
-//            for(int i=0; i<requestDto.getTag().size(); i++){
-//                PostTag tag = new PostTag(post.getId(), requestDto.getTag().get(i));
-//                postTagRepository.save(tag);
-//            }
-//
-//        }
-//        else {
-//            // 없으면
-//            Spot spot = new Spot(requestDto.getLatitude(), requestDto.getLongitude());
-//            Post post = new Post(requestDto, spotRepository.save(spot).getId(), jwtTokenProvider.getUserEmail(token));
-//            Post responsePost = postRepository.save(post);
-//            for(int i=0; i<requestDto.getTag().size(); i++){
-//                PostTag tag = new PostTag(post.getId(), requestDto.getTag().indexOf(i) + 2);
-//                postTagRepository.save(tag);
-//            }
-//
-//
-//        }
+
+        }
         JSONResponse<Post> response = new JSONResponse<>();
         response.setStatusCode(200);
         response.setMessage("글 작성 성공");
         response.setData(null);
         return response;
 
+    }
+
+    @GetMapping("/post/mypage")
+    public JSONResponse<List<Post>> createPosts(@RequestHeader("X-AUTH-TOKEN") String token){
+
+        //List<Post> personalPost = postRepository.findByEmail(jwtTokenProvider.getUserEmail(token));
+
+        JSONResponse<List<Post>> response = new JSONResponse<>();
+        response.setStatusCode(200);
+        response.setMessage("글 불러오기 성공");
+        response.setData(postRepository.findByWriter(jwtTokenProvider.getUserEmail(token)));
+        return response;
     }
 
 }
--- CapturedMomentServer/src/main/java/com/moment/CapturedMomentServer/repository/PostRepository.java
+++ CapturedMomentServer/src/main/java/com/moment/CapturedMomentServer/repository/PostRepository.java
@@ -1,9 +1,17 @@
 package com.moment.CapturedMomentServer.repository;
 
 import com.moment.CapturedMomentServer.domain.Post;
+import com.moment.CapturedMomentServer.domain.Spot;
+import com.moment.CapturedMomentServer.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    public List<Post> findByWriter(String email);
 }
